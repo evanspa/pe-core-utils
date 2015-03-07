a7eb791e5d60ff7edfcdce09d9e536d07ad732ae
Paul Evans <evansp2@gmail.com>
2015-03-07 00:08:05 -0500

    Version 0.0.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pe-core-utils "0.0.5"
+(defproject pe-core-utils "0.0.6-SNAPSHOT"
   :description "A Clojure library providing a set of helper functions."
   :url "https://github.com/evanspa/pe-core-utils"
   :license {:name "MIT"
